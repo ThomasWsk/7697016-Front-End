89ccd85a466b151203aadff2dc69aab4fc5b0779
Thomas <thomasbaracani@live.fr>
2022-09-16 18:34:48 +0200

    "#"

--- avis.js
+++ avis.js
@@ -1,32 +1,3 @@
-const ampoule = pieces[0];
-const imageElement = document.createElement("img");
-imageElement.src = ampoule.img;
-
-const nomElement = document.createElement("h2");
-nomElement.innerText = ampoule.nom;
-
-const prixElement = document.createElement("p");
-prixElement.innerText = ampoule.prix;
-
-const categorieElement = document.createElement("p");
-categorieElement.innerText = ampoule.categorie;
-
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-
-sectionFiches.innerText = "klfkldjfkldsjlkfjdsdsdsdsdslsd";
-
-
-
-
-
-
-
-
-
 /* global Chart */
 
 // Fonction qui enregistre des event listener sur les boutons de la page.
--- pieces.js
+++ pieces.js
@@ -196,3 +196,25 @@ inputPrixMax.addEventListener("input", function () {
 	document.querySelector(".fiches").innerHTML = "";
 	genererPieces(piecesFiltrees);
 });
+
+
+const ampoule = pieces[0];
+const imageElement = document.createElement("img");
+imageElement.src = ampoule.img;
+
+const nomElement = document.createElement("h2");
+nomElement.innerText = ampoule.nom;
+
+const prixElement = document.createElement("p");
+prixElement.innerText = ampoule.prix;
+
+const categorieElement = document.createElement("p");
+categorieElement.innerText = ampoule.categorie;
+
+const sectionFiches = document.querySelector(".fiches");
+sectionFiches.appendChild(imageElement);
+sectionFiches.appendChild(nomElement);
+sectionFiches.appendChild(prixElement);
+sectionFiches.appendChild(categorieElement);
+
+sectionFiches.innerText = "klfkldjfkldsjlkfjdsdsdsdsdslsd";
