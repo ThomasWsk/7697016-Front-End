ad56fc948bac51dfa6d834ff51f93442365b9f25
Thomas <thomasbaracani@live.fr>
2022-09-24 20:27:13 +0200

    update

--- avis.js
+++ avis.js
@@ -55,38 +55,37 @@ formulaireAvis.addEventListener("submit", function (event){
 	});
 });
 
-// Calcul nombre total de commentaires par quantité d'étoiles attribuées
+// Calcul du nombre total de commentaires par quantité d'étoiles attribuées
 const avis = await fetch("http://localhost:8081/avis").then(avis => avis.json());
 const nb_commentaires = [0,0,0,0,0];
 
-for(let commentaire of avis)[
-	nb_commentaires[commentaire.nbEtoiles - 1]++,
-]
+for (let commentaire of avis) {
+    nb_commentaires[commentaire.nbEtoiles - 1]++;
+}
 
 const labels = ["5", "4", "3", "2", "1"];
 
-// Données du graphique
+// Données et personnalisation du graphique
 const data = {
-	labels: labels,
-	datasets: [{
-		label:"Etoiles attribuées",
-		data: nb_commentaires.reverse(),
-		backgroundColor: "rgba(255, 230, 0, 1",
-	}],
-
+    labels: labels,
+    datasets: [{
+      label: "Étoiles attribuées",
+      data: nb_commentaires.reverse(),
+       backgroundColor: "rgba(255, 230, 0, 1)", // couleur jaune
+    }],
 };
 
-// OBJ de configuration final
-const config = { 
-	type: "bar",
-	data: data,
-	options:{
-		indexAxis: "y",		
-	},
-}
+// Objet de configuration final
+const config = {
+    type: "bar",
+    data: data,
+    options: {
+        indexAxis: "y",
+    },
+};
 
-// Rendu graphique dans l'element canvas
-const graphiqueAvis = new Chart(
-	document.querySelector("#graphique-avis"),
-	config,
+// Rendu du graphique dans l'élément canvas
+new Chart(
+    document.querySelector("#graphique-avis"),
+    config,
 );
